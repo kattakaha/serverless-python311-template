3043ab30973090785f69a76839f52447bb2e0096
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-05-10 23:15:03 +0900

    :green_heart: add pytest for config settings

--- tests/test_v1/test_config/test_settings.py
+++ tests/test_v1/test_config/test_settings.py
@@ -3,5 +3,28 @@ from config import settings
 
 
 def test_stage() -> None:
+    assert isinstance(settings.STAGE, str)
     assert settings.STAGE == "dev"
     assert settings.STAGE != "prod"
+
+
+def test_api_cors_allowed_origins() -> None:
+    assert isinstance(settings.APP_API_CORS_ALLOWED_ORIGINS, str)
+    assert settings.APP_API_CORS_ALLOWED_ORIGINS == "http://localhost:3000"
+    assert settings.APP_API_CORS_ALLOWED_ORIGINS != "http://localhost:3001"
+    assert settings.APP_API_CORS_ALLOWED_ORIGINS is not None
+
+
+def test_api_cors_allowed_origin_list() -> None:
+    assert settings.APP_API_CORS_ALLOWED_ORIGIN_LIST == ["http://localhost:3000"]
+    assert "http://localhost:3000" in settings.APP_API_CORS_ALLOWED_ORIGIN_LIST
+    assert isinstance(settings.APP_API_CORS_ALLOWED_ORIGIN_LIST, list)
+    assert settings.APP_API_CORS_ALLOWED_ORIGIN_LIST != ["http://localhost:3001"]
+    assert settings.APP_API_CORS_ALLOWED_ORIGIN_LIST is not None
+
+
+def test_app_api_base_url() -> None:
+    assert isinstance(settings.APP_API_BASE_URL, str)
+    assert settings.APP_API_BASE_URL == "http://localhost:3333"
+    assert settings.APP_API_BASE_URL != "http://localhost:3334"
+    assert settings.APP_API_BASE_URL is not None
