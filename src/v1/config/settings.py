795fe194f07328faeb3c4fa8cf9570dcba4f5b34
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-06-05 08:28:40 +0900

    💚 Fixed api config pytest

--- src/v1/config/settings.py
+++ src/v1/config/settings.py
@@ -6,5 +6,7 @@ STAGE: str = os.getenv("STAGE", "dev")
 
 API_VERSION_HASH = os.getenv("API_VERSION_HASH", default="latest")
 
-API_CORS_ALLOWED_ORIGINS: str = os.getenv("API_CORS_ALLOWED_ORIGINS", default="http://localhost:3000")
+API_CORS_ALLOWED_ORIGINS: str = os.getenv(
+    "API_CORS_ALLOWED_ORIGINS", default="http://localhost:3000,http://localhost:3005"
+)
 API_CORS_ALLOWED_ORIGIN_LIST: list[str] = API_CORS_ALLOWED_ORIGINS.split(",")
--- tests/test_v1/test_config/test_settings.py
+++ tests/test_v1/test_config/test_settings.py
@@ -9,22 +9,16 @@ def test_stage() -> None:
 
 
 def test_api_cors_allowed_origins() -> None:
-    assert isinstance(settings.APP_API_CORS_ALLOWED_ORIGINS, str)
-    assert settings.APP_API_CORS_ALLOWED_ORIGINS == "http://localhost:3000"
-    assert settings.APP_API_CORS_ALLOWED_ORIGINS != "http://localhost:3001"
-    assert settings.APP_API_CORS_ALLOWED_ORIGINS is not None
+    assert isinstance(settings.API_CORS_ALLOWED_ORIGINS, str)
+    assert settings.API_CORS_ALLOWED_ORIGINS == "http://localhost:3000,http://localhost:3005"
+    assert settings.API_CORS_ALLOWED_ORIGINS != "http://localhost:3001"
+    assert settings.API_CORS_ALLOWED_ORIGINS is not None
 
 
 def test_api_cors_allowed_origin_list() -> None:
-    assert settings.APP_API_CORS_ALLOWED_ORIGIN_LIST == ["http://localhost:3000"]
-    assert "http://localhost:3000" in settings.APP_API_CORS_ALLOWED_ORIGIN_LIST
-    assert isinstance(settings.APP_API_CORS_ALLOWED_ORIGIN_LIST, list)
-    assert settings.APP_API_CORS_ALLOWED_ORIGIN_LIST != ["http://localhost:3001"]
-    assert settings.APP_API_CORS_ALLOWED_ORIGIN_LIST is not None
-
-
-def test_app_api_base_url() -> None:
-    assert isinstance(settings.APP_API_BASE_URL, str)
-    assert settings.APP_API_BASE_URL == "http://localhost:3333"
-    assert settings.APP_API_BASE_URL != "http://localhost:3334"
-    assert settings.APP_API_BASE_URL is not None
+    assert settings.API_CORS_ALLOWED_ORIGIN_LIST == ["http://localhost:3000", "http://localhost:3005"]
+    assert "http://localhost:3000" in settings.API_CORS_ALLOWED_ORIGIN_LIST
+    assert "http://localhost:3005" in settings.API_CORS_ALLOWED_ORIGIN_LIST
+    assert isinstance(settings.API_CORS_ALLOWED_ORIGIN_LIST, list)
+    assert settings.API_CORS_ALLOWED_ORIGIN_LIST != ["http://localhost:3001"]
+    assert settings.API_CORS_ALLOWED_ORIGIN_LIST is not None
